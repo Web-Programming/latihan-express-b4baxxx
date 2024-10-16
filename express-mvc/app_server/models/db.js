4a3d3bcfd02f1548ace4c8b9a98f42f55fabdb28
oliviaaja1 <160828060+b4baxxx@users.noreply.github.com>
2024-10-16 09:26:47 +0700

    mongo db

---
+++ express-mvc/app_server/models/db.js
@@ -0,0 +1,17 @@
+let mongoose = require("mongoose");
+let dbuRI = "mongodb://localhost:27017/pawII-si5c";
+
+mongoose.connect(dbuRI, {
+    //userNEWUrlParser: true
+});
+mongoose.connection.on("connection",() => {
+    console.log("Connected to mongoDB");
+
+});
+mongoose.connection.on("eror", (error) => {
+    console.log("Connection Eror :" + error);
+
+});
+mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected From Mongodb");
+});
